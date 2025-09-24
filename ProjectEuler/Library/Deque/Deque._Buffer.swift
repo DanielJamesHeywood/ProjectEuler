8e490b67722adaa9e1cf2e63f5e3504b0a78709d
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-24 14:39:19 +0100

    Declared count variable for _Buffer.

--- ProjectEuler/Library/Deque/Deque._Buffer.swift
+++ ProjectEuler/Library/Deque/Deque._Buffer.swift
@@ -2,5 +2,15 @@
 extension Deque {
     
     @usableFromInline
-    internal class _Buffer: ManagedBuffer<_Header, Element> {}
+    internal class _Buffer: ManagedBuffer<_Header, Element> {
+        
+        @inlinable
+        internal var count: Int {
+            get {
+                return withUnsafeMutablePointerToHeader { pointer in
+                    return pointer.pointee.count
+                }
+            }
+        }
+    }
 }
