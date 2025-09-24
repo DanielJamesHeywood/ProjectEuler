acebc5fdacf9f20428b32977e9e27d6842ecc49c
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-24 14:43:10 +0100

    Removed count variable for structural reasons.

--- ProjectEuler/Library/Deque/Deque._Buffer.swift
+++ ProjectEuler/Library/Deque/Deque._Buffer.swift
@@ -2,15 +2,5 @@
 extension Deque {
     
     @usableFromInline
-    internal class _Buffer: ManagedBuffer<_Header, Element> {
-        
-        @inlinable
-        internal var count: Int {
-            get {
-                return withUnsafeMutablePointerToHeader { pointer in
-                    return pointer.pointee.count
-                }
-            }
-        }
-    }
+    internal class _Buffer: ManagedBuffer<_Header, Element> {}
 }
