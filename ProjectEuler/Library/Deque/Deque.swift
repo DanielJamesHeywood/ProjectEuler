882fa821e6df5f0054675784c194ca454f87786d
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-25 21:58:08 +0100

    Removed initialiser for more structured approach to expanding the structure.

--- ProjectEuler/Library/Deque/Deque.swift
+++ ProjectEuler/Library/Deque/Deque.swift
@@ -3,9 +3,4 @@ public struct Deque<Element> {
     
     @usableFromInline
     internal var _buffer: _Buffer
-    
-    @inlinable
-    public init(minimumCapacity: Int) {
-        self._buffer = .create(minimumCapacity: minimumCapacity)
-    }
 }
