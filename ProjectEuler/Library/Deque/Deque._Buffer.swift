e94cb807373183c8f1e562f4e70342caaa35ad10
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-25 11:18:36 +0100

    Declared create function for _Buffer.

--- ProjectEuler/Library/Deque/Deque._Buffer.swift
+++ ProjectEuler/Library/Deque/Deque._Buffer.swift
@@ -8,3 +8,14 @@ extension Deque {
         deinit {}
     }
 }
+
+extension Deque._Buffer {
+    
+    @inlinable
+    internal static func create(minimumCapacity: Int) -> Deque._Buffer {
+        let buffer = Deque._Buffer.create(minimumCapacity: minimumCapacity) { buffer in
+            return Deque._Header(capacity: buffer.capacity)
+        }
+        return unsafeDowncast(buffer, to: Deque._Buffer.self)
+    }
+}
