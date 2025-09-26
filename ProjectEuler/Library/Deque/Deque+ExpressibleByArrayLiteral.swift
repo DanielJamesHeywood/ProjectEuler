f031c2622f03cac7fb473186c4423865408967e1
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-26 22:51:23 +0100

    Stylistic changes to ExpressibleByArrayLiteral.

--- ProjectEuler/Library/Deque/Deque+ExpressibleByArrayLiteral.swift
+++ ProjectEuler/Library/Deque/Deque+ExpressibleByArrayLiteral.swift
@@ -1,8 +1,6 @@
 
 extension Deque: ExpressibleByArrayLiteral {
     
-    public typealias ArrayLiteralElement = Element
-    
     @inlinable
     public init(arrayLiteral elements: Element...) {
         self.init(elements)
