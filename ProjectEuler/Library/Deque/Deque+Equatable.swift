60083139f31357c24c1f0918e36a9087dc9637f1
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-27 21:25:03 +0100

    Solved issues #3 and #4.

--- ProjectEuler/Library/Deque/Deque+Equatable.swift
+++ ProjectEuler/Library/Deque/Deque+Equatable.swift
@@ -3,6 +3,9 @@ extension Deque: Equatable where Element: Equatable {
     
     @inlinable
     public static func == (lhs: Deque, rhs: Deque) -> Bool {
+        guard lhs._buffer !== rhs._buffer else {
+            return true
+        }
         guard lhs.count == rhs.count else {
             return false
         }
