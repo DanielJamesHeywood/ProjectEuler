cb3a0ede1d7d1b9a16c2ff3c76c086af1e2f5bd7
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-30 20:42:58 +0100

    Implemented initializer for Deque's iterator.

--- ProjectEuler/Library/Deque/Deque.Iterator/Deque.Iterator.swift
+++ ProjectEuler/Library/Deque/Deque.Iterator/Deque.Iterator.swift
@@ -2,5 +2,14 @@
 extension Deque {
     
     @frozen
-    public struct Iterator {}
+    public struct Iterator {
+        
+        @usableFromInline
+        internal let _baseStorage: _Storage
+        
+        @inlinable
+        internal init(_base: Deque) {
+            _baseStorage = _base._storage
+        }
+    }
 }
