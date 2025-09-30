2777254f1237420774ef1bc89fbb631583c7540f
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-30 21:46:50 +0100

    Declared and implemented IteratorProtocol for Deque's iterator.

---
+++ ProjectEuler/Library/Deque/Deque.Iterator/Deque.Iterator+IteratorProtocol.swift
@@ -0,0 +1,17 @@
+
+extension Deque.Iterator: IteratorProtocol {
+    
+    /// Advances to the next element and returns it, or `nil` if no next element exists.
+    ///
+    /// - Returns: The next element in the deque, if a next element exists; otherwise, `nil`.
+    @inlinable
+    public mutating func next() -> Element? {
+        guard _nextIndex != _endIndex else {
+            return nil
+        }
+        defer {
+            _nextIndex += 1
+        }
+        return _baseStorage[_nextIndex]
+    }
+}
