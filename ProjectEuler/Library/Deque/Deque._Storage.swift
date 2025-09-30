07ccbf6d037a0a50dc4088854ca044679f666de0
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-30 20:27:10 +0100

    Declared initializer for Deque and implemented _Storage to facilitate it.

--- ProjectEuler/Library/Deque/Deque._Storage.swift
+++ ProjectEuler/Library/Deque/Deque._Storage.swift
@@ -2,5 +2,36 @@
 extension Deque {
     
     @usableFromInline
-    internal final class _Storage {}
+    internal final class _Storage {
+        
+        @usableFromInline
+        internal var _capacity: Int
+        
+        @usableFromInline
+        internal var _offset = 0
+        
+        @usableFromInline
+        internal var _count = 0
+        
+        @usableFromInline
+        internal var _pointerToElements: UnsafeMutablePointer<Element>
+        
+        @inlinable
+        internal init(capacity: Int) {
+            _capacity = capacity
+            _pointerToElements = .allocate(capacity: capacity)
+        }
+        
+        @inlinable
+        deinit {
+            if _offset + _count <= _capacity {
+                _pointerToElements.advanced(by: _offset).deinitialize(count: _count)
+            } else {
+                let aCount = _capacity - _offset
+                _pointerToElements.advanced(by: _offset).deinitialize(count: aCount)
+                _pointerToElements.deinitialize(count: _count - aCount)
+            }
+            _pointerToElements.deallocate()
+        }
+    }
 }
--- ProjectEuler/Library/Deque/Deque.swift
+++ ProjectEuler/Library/Deque/Deque.swift
@@ -4,4 +4,12 @@ public struct Deque<Element> {
     
     @usableFromInline
     internal var _storage: _Storage
+    
+    /// Creates an empty deque with preallocated space for at least the specified number of elements.
+    ///
+    /// - Parameter minimumCapacity: The minimum number of elements that the newly created deque should be able to store without reallocating its storage.
+    @inlinable
+    public init(minimumCapacity: Int) {
+        _storage = _Storage(capacity: minimumCapacity)
+    }
 }
