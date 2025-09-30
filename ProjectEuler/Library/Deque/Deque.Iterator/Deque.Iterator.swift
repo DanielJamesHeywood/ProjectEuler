aa89802988d85fcf665ecb93126751d0f1945c90
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-30 21:18:14 +0100

    Extended Deque's iterator to facilitate a conformance to IteratorProtocol, Iterator has been written to also serve as an iterator over a subsequence of a Deque.

--- ProjectEuler/Library/Deque/Deque.Iterator/Deque.Iterator.swift
+++ ProjectEuler/Library/Deque/Deque.Iterator/Deque.Iterator.swift
@@ -7,9 +7,17 @@ extension Deque {
         @usableFromInline
         internal let _baseStorage: _Storage
         
+        @usableFromInline
+        internal var _nextIndex: Int
+        
+        @usableFromInline
+        internal let _endIndex: Int
+        
         @inlinable
         internal init(_base: Deque) {
             _baseStorage = _base._storage
+            _nextIndex = _base.startIndex
+            _endIndex = _base.endIndex
         }
     }
 }
